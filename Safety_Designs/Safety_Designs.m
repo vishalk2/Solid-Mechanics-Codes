127f3293bb79a0252538e287fba5cb79e9e9f6ba
K Badari Vishal <68963724+vishalk2@users.noreply.github.com>
2020-12-05 12:13:17 +0530

    Update Safety_Designs.m

--- Safety_Designs/Safety_Designs.m
+++ Safety_Designs/Safety_Designs.m
@@ -3,22 +3,27 @@ clear all; clc
 %Enter units in SI
 
 %Loads
-D = 25000; %Dead Load in N
-L = 60000; %Live Load in N
-W = 30000; %Wind Load in N
+D = 27.2*(10^3); %Dead Load in N
+L = 60.1*(10^3); %Live Load in N
+W = 30.1*(10^3); %Wind Load in N
 
 %Other parameters
-Y = 250*(10^6); %Yield Strength in Pa
-SF = 5/3; %Factor of Safety
-phi = 0.9; %Resistance factor
+Y = 241.5*(10^6); %Yield Strength in Pa
+SF = 1; %Factor of Safety
+phi = 1; %Resistance factor
 
 %Factor of Safety Design
 Q1 = D+L+W %Total Service-level Load in N
 Ag1 = (10^6)*Q1*SF/Y %Required Area in mm^2
 d1 = (4*Ag1/pi)^0.5 %Required Diameter in mm
 
+%Weights
+d = 4.2;
+l = 4.8;
+w = 4.2;
+
 %Limit States Design
-LC = [1.4*D;1.2*D+1.6*L;1.2*D+0.5*L+1.6*W] %Load combinations
+LC = [d*D;d*D+l*L;d*D+l*L+w*W] %Load combinations
 Q2 = max(LC) %Total Load effect in N
 Ag2 = (10^6)*Q2/(phi*Y) %Required Area in mm^2
 d2 = (4*Ag2/pi)^0.5 %Required Diamter in mm
