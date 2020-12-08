da25e040a928772d8422a82309f9d88c78790834
K Badari Vishal <68963724+vishalk2@users.noreply.github.com>
2020-12-08 22:53:14 +0530

    Update Stress_on_an_arbitrary_plane.m

--- Stresses/Stress_on_an_arbitrary_plane.m
+++ Stresses/Stress_on_an_arbitrary_plane.m
@@ -1,33 +1,30 @@
 clear all; clc
 %Stress components
-sigma_xx = 50;
-sigma_yy = -30;
+sigma_xx = 100;
+sigma_yy = 60;
 sigma_zz = 20;
-sigma_xy = 5;
-sigma_yz = 0;
-sigma_xz = -30;
+sigma_xy = 40;
+sigma_yz = 80;
+sigma_xz = 0;
 T = [sigma_xx,sigma_xy,sigma_xz;...
     sigma_xy,sigma_yy,sigma_yz;...
     sigma_xz,sigma_yz,sigma_zz]
 
 %Direction cosines & Normal Vector
-l = 1/sqrt(3);
-m = 1/sqrt(3);
-n = 1/sqrt(3);
+l = 0.8;
+m = 0.6;
+n = 0;
 N = [l m n]'
 
 %Stress acting on an arbtrary plane
-syms sigma_Px sigma_Py sigma_Pz
-sigma_p = [sigma_Px;sigma_Py;sigma_Pz] == T*N
 sigma_P = T*N
+sigma_P_mag = norm(sigma_P) %Magnitude
+sigma_Px = sigma_P(1)
+sigma_Py = sigma_P(2)
+sigma_Pz = sigma_P(3)
 
 %Normal Stress acting on an arbtrary plane
-syms sigma_PN
-sigma_normal = sigma_PN == (T*N)'*N
-sigma_pn = (T*N)'*N
+sigma_PN = (T*N)'*N
 
 %Shear stress acting on an arbitrary plane
-syms sigma_PS
-sigma_shear = sigma_PS == sqrt(sigma_Px^2 + sigma_Py^2 + sigma_Pz^2 - sigma_PN^2)
-sigma_ps = sqrt(sigma_P(1)^2 + sigma_P(2)^2 + sigma_P(3)^2 - sigma_pn^2)
-
+sigma_PS = sqrt(sigma_Px^2 + sigma_Py^2 + sigma_Pz^2 - sigma_PN^2)
